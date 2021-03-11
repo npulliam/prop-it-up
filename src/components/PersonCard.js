36d8daa9d3d66a2038a2ae445c1f4025c2ef0074
npulliam <nathanpulliam@gmail.com>
2021-03-11 12:43:17 -0800

    destructuring in PersonCard to reduce repetition

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -18,14 +18,16 @@ class PersonCard extends React.Component {
     }
 
     render() {
+        const {firstName, lastName, hairColor} = this.props
+        const {age} = this.state
         return (
             <div className="container">
                 <div className="row">
                     <div className="card col bg-dark text-light mx-4">
-                        <h2>{this.props.lastName}, {this.props.firstName}</h2>
-                        <p>Age: {this.state.age}</p>
-                        <p>Hair Color: {this.props.hairColor}</p>
-                        <button onClick={this.birthdayClick} className="btn btn-danger w-25" >{this.props.firstName} {this.props.lastName}'s {this.state.age + 1} birthday!</button>
+                        <h2>{lastName}, {firstName}</h2>
+                        <p>Age: {age}</p>
+                        <p>Hair Color: {hairColor}</p>
+                        <button onClick={this.birthdayClick} className="btn btn-danger w-25" >{firstName} {lastName}'s {age + 1} birthday!</button>
                     </div>
                 </div>
             </div>
