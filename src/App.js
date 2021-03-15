568c6fadb5d3804c938407b48cbd71ca2c583526
npulliam <nathanpulliam@gmail.com>
2021-03-15 13:44:37 -0700

    remove button added and checkbox for completed

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import './App.css';
 import Tabs from './components/Tabs';
 import ToDo from './components/ToDo';
 import TaskForm from './components/TaskForm';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
-
-  const [tasks, setTasks] = useState([
+  const initTasks = [
     {text: "Get Python Red Belt", completed: true},
     {text: "Get Python Black Belt", completed: true},
     {text: "Get MERN Black Belt", completed: false},
     {text: "Turn into checklist", completed: false},
     {text: "Add remove button", completed: false}
-  ]);
+  ]
+
+  const [tasks, setTasks] = useState([...initTasks]);
   
+
   const addTask = ( newTask ) => {
     setTasks([...tasks, newTask]);
   }
 
+
   return (
     <div className="App">
       <TaskForm onNewTask={ addTask }/>
-      <ToDo tasks={ tasks }/>
+      <ToDo tasks={ tasks } setTasks={ setTasks }/>
     </div>
     
   );
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const ToDo = (props) => {
-    const tasks = props.tasks.map((task, i) => {
+    
+    const toggleComplete = (taskIdx) => {
+        // const tasksCopy = [...props.tasks]
+        props.tasks[taskIdx].completed = !props.tasks[taskIdx].completed;
+        props.setTasks([...props.tasks])
+    }
+
+    const deleteTask = (taskIdx) => {
+        props.tasks.splice(taskIdx)
+        props.setTasks([...props.tasks])
+    }
+
+    const tasksHtml = props.tasks.map((task, i) => {
         return(
-            task.completed ?
-            <p key={i} className=""><del>{task.text}</del></p> :
-            <p key={i}>{task.text}</p>          
+            <div key={i} className="d-flex">{
+                task.completed ?
+                <p className="test"><del>{task.text}</del></p> :
+                <p>{task.text}</p>
+            }       
+                <input type="checkbox" className="form-check-input mx-3" checked={task.completed} onChange={ (e) => toggleComplete(i)}/>
+                <button className="btn-sm btn-danger" onClick={(e) => {deleteTask(i)}}>Delete Task</button>
+            </div>
         )
     });
 
@@ -13,7 +30,7 @@ const ToDo = (props) => {
         <div className="container my-4 p-3">
             <div className="row">
                 <div className="col">
-                    {tasks}
+                    {tasksHtml}
                 </div>
             </div>
         </div>
